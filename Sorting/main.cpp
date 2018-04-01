a728294d89d0ca4900094fd66022b145e4b3a6ef
James Hudspeth <unit001@ymail.com>
2018-04-01 16:23:38 -0700

    Initial Commit

---
+++ Sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorting
+//
+//  Created by James D. Hudspeth on 4/1/18.
+//  Copyright © 2018 James D. Hudspeth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
